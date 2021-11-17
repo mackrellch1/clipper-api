7cb4340e8c51545ee4a85d221c6390c569edadef
cmackrell <colin.mackrell99@gmail.com>
2021-11-16 21:32:42 -0500

    query with clip duration

--- src/routes/random.route.ts
+++ src/routes/random.route.ts
@@ -2,7 +2,16 @@ import * as express from 'express';
 import { RecordingModel } from '../models/recording.model';
 
 export default async function randomRoute(req: express.Request, res: express.Response) {
-    const doc = await RecordingModel.aggregate([{ $sample: { size: 1 } }])
+    const doc = await RecordingModel.aggregate([
+        { 
+            $match: {
+                clipDuration: {$gt: 2000}
+            }
+        },
+        { 
+            $sample: { size: 1 } 
+        }
+    ])
     
     return res.json(doc)
 }
